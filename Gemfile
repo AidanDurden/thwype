5630cd48b5bb986c51dcda93cf50ed4c60591d32
Aidan Durden <aidan.durden@gmail.com>
2013-06-11 09:56:21 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+gem 'bootstrap-sass'
+
+gem "jquery-rails", "2.3.0"
+gem 'jquery-ui-rails'
+
+#gem 'activeadmin'
+#gem "meta_search",    '>= 1.1.0.pre'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'annotate'
+end
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'nokogiri'
+end
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thwype::Application.config.secret_token = '8084254f3bb523313ea978b2c69dcb9098473ec020e5d5d927cc2ca47a9be51a696002962f037d15bd6424091f64c40bb193df43571a97dc3708155d89b566a0'
